f6567961bb81c4cc374e47ed55c42b14dacb704b
Zoki <zoran.lazic@beenius.tv>
2018-01-23 16:47:37 +0100

    Adding logging middleware

--- main.go
+++ main.go
@@ -2,35 +2,57 @@ package main
 
 import (
 	"context"
-	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
 
-// ----
-
-// Endpoint tpye
-type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+// ------------------------------------------------
+// Middleware type
+type Middleware func(endpoint.Endpoint) endpoint.Endpoint
 
 // ------------------------------------------------
 
+func loggingMiddleware(logger log.Logger) Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, request interface{}) (interface{}, error) {
+			logger.Log("msg", "calling endpoint")
+			defer logger.Log("msg", "called endpoint")
+			return next(ctx, request)
+		}
+	}
+}
+
 func main() {
+	logger := log.NewLogfmtLogger(os.Stderr)
+
 	svc := stringService{}
 
+	var uppercase endpoint.Endpoint
+	uppercase = makeUppercaseEndpoint(svc)
+	uppercase = loggingMiddleware(log.With(logger, "method", "uppercase"))(uppercase)
+
+	var count endpoint.Endpoint
+	count = makeCountEndpoint(svc)
+	count = loggingMiddleware(log.With(logger, "method", "count"))(count)
+
 	uppercaseHandler := httptransport.NewServer(
-		makeUppercaseEndpoint(svc),
+		uppercase,
 		decodeUppercaseRequest,
 		encodeResponse,
 	)
 
 	countHandler := httptransport.NewServer(
-		makeCountEndpoint(svc),
+		count,
 		decodeCountRequest,
 		encodeResponse,
 	)
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	logger.Log("msg", "HTTP", "addr", ":8080")
+	logger.Log("err", http.ListenAndServe(":8080", nil))
 }
